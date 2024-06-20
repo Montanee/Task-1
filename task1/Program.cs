92ca73c6b862e624d5a91e9c488834abd2fe6460
Administrator <Administrator@WIN-S09DU0P86JK>
2024-06-20 16:30:38 +0300

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Выберите опцию: ");
+        Console.WriteLine("1. Конвертировать из Цельсия в Фаренгейт");
+        Console.WriteLine("2. Конвертировать из Фаренгейта в Цельсия");
+        int выбор = Convert.ToInt32(Console.ReadLine());
+
+        if (выбор == 1)
+        {
+            Console.Write("Введите температуру в Цельсиях: ");
+            double температураВЦельсиях = Convert.ToDouble(Console.ReadLine());
+            double температураВФаренгейтах = (температураВЦельсиях * 9 / 5) + 32;
+            Console.WriteLine("Температура в Фаренгейтах: " + температураВФаренгейтах);
+        }
+        else if (выбор == 2)
+        {
+            Console.Write("Введите температуру в Фаренгейтах: ");
+            double температураВФаренгейтах = Convert.ToDouble(Console.ReadLine());
+            double температураВЦельсиях = (температураВФаренгейтах - 32) * 5 / 9;
+            Console.WriteLine("Температура в Цельсиях: " + температураВЦельсиях);
+        }
+        else
+        {
+            Console.WriteLine("Неверный выбор.");
+        }
+    }
+}
